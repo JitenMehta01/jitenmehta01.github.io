16b49268849481fb083ab5a9548c527443bc82b3
Jiten Mehta <jitenmehta03@gmail.com>
2020-07-10 10:48:25 +0100

    Working on MobileMenu function

--- script.js
+++ script.js
@@ -4,4 +4,13 @@ const mobileMenu = document.querySelector('.mobileMenu')
 
 toggleButton.addEventListener('click', e =>{
   mobileMenu.classList.toggle('menu-open');
+});
+
+
+ // closes menu when clicked outside
+
+window.addEventListener('click', e =>{
+  if(mobileMenu.classList[5] === 'menu-open' && e.target.className === 'main-content'){
+  mobileMenu.classList.remove('menu-open')
+  }
 })
